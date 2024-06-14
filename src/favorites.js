6d7fc4c954de15ceeef5f3286048d8bb4604d07b
Cristina Bubu <cristinaflorinamihalcea@hmail.com>
2024-06-14 05:06:48 +0300

    virgule

--- src/favorites.js
+++ src/favorites.js
@@ -1,7 +1,7 @@
-import './components/header-cristina/Header.js'
-import './components/header-cristina/header-menu.js'
-import './components/favorites-george/Favorites.js'
-import './components/OrderNow/OrderNow.js'
-import './components/go_up-catalin/go_up-catalin.js'
-import './components/recipe_modal-alina/modal_recipe.js'
-import './components/footer-cristina/footer_modal-cristina.js'
+import './components/header-cristina/Header.js';
+import './components/header-cristina/header-menu.js';
+import './components/favorites-george/Favorites.js';
+import './components/OrderNow/OrderNow.js';
+import './components/go_up-catalin/go_up-catalin.js';
+import './components/recipe_modal-alina/modal_recipe.js';
+import './components/footer-cristina/footer_modal-cristina.js';
