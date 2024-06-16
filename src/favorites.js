2e0f9770220f63fdd684d4533890c476e27718f0
Cristina Bubu <cristinaflorinamihalcea@hmail.com>
2024-06-16 20:37:29 +0300

    dezactivat cateva importuri

--- src/favorites.js
+++ src/favorites.js
@@ -1,11 +1,11 @@
 import './partials/header-cristina/dark-mode.js';
 import './partials/header-cristina/mobile-menu.js';
 import './partials/favorites-george/favorites.js';
-import './partials/hero_modal-ramon/OrderNow.js';
+//import './partials/hero_modal-ramon/OrderNow.js';
 import './partials/go_up-catalin/go_up-catalin.js';
 import './partials/recipe_modal-alina/modal_recipe.js';
 import './partials/rating_modal-alina/rating.js';
 import './partials/rating_modal-alina/rating_api.js';
-import './partials/footer-cristina/footer_modal-cristina.js';
+//import './partials/footer-cristina/footer_modal-cristina.js';
 import './partials/pagination-catalin/pagination.js';
 import './partials/modal-footer-echipa/modal-footer-echipa.js';
--- src/index.js
+++ src/index.js
@@ -8,7 +8,10 @@ import './partials/hero_modal-ramon/OrderNow.js';
 import './partials/recipe_modal-alina/modal_recipe.js';
 import './partials/go_up-catalin/go_up-catalin.js';
 import './partials/rating_modal-alina/rating.js';
-import './partials/footer-cristina/footer_modal-cristina.js';
+//import './partials/footer-cristina/footer_modal-cristina.js';
 import './partials/header-cristina/dark-mode.js';
 import './partials/popular_recipes-aimee/popular_recipes.js';
 import './partials/modal-footer-echipa/modal-footer-echipa.js';
+
+
+
